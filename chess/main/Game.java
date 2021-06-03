1610c0f95288980d908d35bfc9bed420e6f3ba75
Matyxus <maty.svadlenka@seznam.cz>
2021-06-03 15:10:17 +0200

    Initial project strucutre

--- chess/main/Game.java
+++ chess/main/Game.java
@@ -1,46 +1,29 @@
-package chess2.main;
+package main;
 
 import java.awt.image.BufferStrategy;
 import java.awt.Graphics;
+/*
+import managers.GraphicsManager;
+import managers.MouseManager;
+import states.BoardPlacementState;
+import states.GameState;
+import states.State;
+import states.ViewerState;
+*/
 
-import chess2.managers.GraphicsManager;
-import chess2.managers.MouseManager;
-import chess2.states.BoardPlacementState;
-import chess2.states.GameState;
-import chess2.states.State;
-import chess2.states.ViewerState;
 
-public class Game implements Runnable {
+public class Game {
     public final int width, height;
     public final String title;
-    //thread
+    
     private boolean running = false;
-    private Thread thread;
     // screen
     private BufferStrategy bs;
     private Graphics g;
-    private Display display;
-    private GraphicsManager graphicsManager;
-    // Input
-    private final MouseManager mouseManager;
-    // States
-    public State gameState;
-    public State BoardPlacementState;
-    public State viewerState;
-
-    private Handler handler;
-    
     public Game(String title, int width, int height) {
         this.width = width;
         this.height = height;
         this.title = title;
-        mouseManager = new MouseManager();
-    }
-
-    private void update() {
-        if (State.getState() != null) {
-            State.getState().tick();
-        }
     }
 
     // draw to screen using Buffer
@@ -64,7 +47,6 @@ public class Game implements Runnable {
     }
 
     // init game + update it accordingly, gets called when start is called
-    @Override
     public void run() {
         // 60 fps setup
         final int TARGET_FPS = 60;
@@ -73,29 +55,27 @@ public class Game implements Runnable {
         long now;
         long wait;
         init();
-        while (running) {// other solutions takes too much CPU to get exact 61fps (using system.nano...)
+        while (running) {
             now = System.nanoTime();
+            System.out.println("In game loop");
+            /*
             if (mouseManager.leftPressed() || mouseManager.rightPressed()) {// check for input
                 update();
             }
             //necessary, game loop is too fast, reset mouse buttons
             mouseManager.setLeftClick(false);
             mouseManager.setRightClick(false);
+            */
             after = System.nanoTime() - now;
-            wait = (OPTIMAL_TIME - after) / 1000000;
-            wait = (wait >= 0 ) ? wait:-wait; // negative vaule in case of wait
+            wait =  Math.abs((OPTIMAL_TIME - after) / 1000000);
             try {
                 Thread.sleep(wait);
                 render();
             } catch (InterruptedException e) {
                 e.printStackTrace();
+                running = false;
             }
-        } 
-        stop();
-    }
-
-    public MouseManager getMouseManager(){
-        return mouseManager;
+        }
     }
 
     // init display
@@ -110,47 +90,5 @@ public class Game implements Runnable {
         graphicsManager = new GraphicsManager(handler);
         BoardPlacementState = new BoardPlacementState(handler);
         State.setState(BoardPlacementState);
-    }
-
-    // init thread, and the whole game
-    public synchronized void start() {
-        if(running){
-            return;
-        }
-        running = true;
-        thread = new Thread(this); // this game class
-        thread.start();
-    }
-
-    public synchronized void stop() { // End thread
-        if(!(running)){
-            return;
-        }
-        running = false;
-        try {
-            thread.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-    //GETTERS && SETTERS
-    public Display getDisplay() {
-        return display;
-    }
-
-    public GraphicsManager getGraphicsManager() {
-        return graphicsManager;
-    }
-
-    public void startViewerState(){
-        this.viewerState = new ViewerState(handler);
-    }
-    
-    public void startGameState() {
-        this.gameState = new GameState(handler);
-    }
-
-    public void startBoardPlacementState(){
-        this.BoardPlacementState = new BoardPlacementState(handler);
     }
 }
